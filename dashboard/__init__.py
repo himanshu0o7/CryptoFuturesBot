8bfcb2cc7e511e52d0a0e25fb466df3d12ba4466
himanshu0o7 <himanshu01988@gmail.com>
2025-07-14 20:13:15 +0530

    Update __init__.py

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,7 +1,7 @@
-"""
-Dashboard module for CryptoFuturesBot
+
+#Dashboard module for CryptoFuturesBot
 Contains web-based dashboard and monitoring interfaces
-"""
+
 
 from .streamlit_dashboard import run_dashboard
 
